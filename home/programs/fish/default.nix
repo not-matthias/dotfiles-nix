dabe60d8ebfa93b9603e64b3d8b2f62bb4b35ce2
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-10-08 22:29:35 +0200

    feat: update fish

--- home/programs/fish/default.nix
+++ home/programs/fish/default.nix
@@ -1,14 +1,35 @@
-{
+{pkgs, ...}: let
+  fishPrompt = builtins.readFile ./prompt.fish;
+  fishAbbr = builtins.readFile ./abbr.fish;
+in {
   programs.fish = {
     enable = true;
-    plugins = [custom.theme fenv];
+    plugins = [
+      {
+        name = "foreign-env";
+        src = pkgs.fishPlugins.foreign-env.src;
+      }
+    ];
     shellAliases = {
-      #      cat = "bat";
-      #      dc = "docker-compose";
-      #      ls = "exa";
-      #      ".." = "cd ..";
-      #      tree = "exa -T";
     };
-    shellInit = fishConfig;
+    shellAbbrs = {
+      "c" = "clear";
+      "x" = "exit";
+      # TODO: Add the others
+
+      "pwd" = "pwd | xclip -selection clipboard && pwd";
+    };
+    interactiveShellInit = fishPrompt + fishAbbr;
+    functions = {
+      fish_greeting = '''';
+      fish_mode_prompt = '''';
+      dir = ''
+        # https://github.com/laughedelic/fish/blob/master/functions/dir.fish
+        function dir -a path -d "Creates subdirectories and jumps inside"
+        	mkdir -p $path
+        	and cd $path
+        end
+      '';
+    };
   };
 }
