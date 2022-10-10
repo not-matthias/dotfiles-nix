533c52f40d6ccffef19628efb3a386f1076e3571
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-10-10 14:43:14 +0200

    feat: add zotero

--- home/home.nix
+++ home/home.nix
@@ -10,6 +10,19 @@
   home.username = "not-matthias";
   home.homeDirectory = "/home/not-matthias";
 
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [
@@ -79,7 +92,6 @@
     obsidian
     anki
     xournalpp
-    blanket
     calibre
     vlc
     krita
@@ -87,6 +99,7 @@
     cava
     notepadqq
     flameshot
+    zotero
 
     # Dev
     jetbrains-mono
@@ -106,19 +119,6 @@
 
   # Ensure fonts installed via Nix are picked up.
   fonts.fontconfig.enable = true;
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "22.05";
 
   imports = (import ./programs) ++ (import ./services);
 }
