c9bbec3ec8dcc306d2521975d44ba6e1c69f7572
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-10-11 17:23:14 +0200

    feat: add flameshot

---
+++ modules/services/flameshot/default.nix
@@ -0,0 +1,17 @@
+{ pkgs, user, ... }:
+{
+  services = {
+    flameshot = {
+      enable = true;
+      settings = {
+        General = {
+          savePath = "/home/${user}/";
+          saveAsFileExtension = ".png";
+          uiColor = "#2d0096";
+          showHelp = "false";
+        #   disabledTrayIcon = "true";
+        };
+      };
+    };
+  };
+}
