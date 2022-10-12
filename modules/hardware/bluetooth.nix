19a7de5b6e66131d4e3382711aed33158a1affde
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-10-12 18:47:48 +0200

    fmt: treefmt

--- modules/hardware/bluetooth.nix
+++ modules/hardware/bluetooth.nix
@@ -1,8 +1,7 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   hardware.bluetooth = {
     enable = true;
-    hsphfpd.enable = true;         # HSP & HFP daemon
+    hsphfpd.enable = true; # HSP & HFP daemon
     settings = {
       General = {
         Enable = "Source,Sink,Media,Socket";
