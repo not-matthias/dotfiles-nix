a3835fb89c3339adb2fbbe3a7947e941fb27cf74
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-10-15 16:52:37 +0200

    feat: enable direnv

--- modules/programs/direnv/default.nix
+++ modules/programs/direnv/default.nix
@@ -3,15 +3,10 @@
   lib,
   config,
   ...
-}:
-with lib; let
-  cfg = config.modules.direnv;
-in {
-  options.modules.direnv = {enable = mkEnableOption "direnv";};
-  config = mkIf cfg.enable {
-    programs.direnv = {
-      enable = true;
-      nix-direnv.enable = true;
-    };
+}: {
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+    enableFishIntegration = true;
   };
 }
