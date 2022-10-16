431bc6222e3146d370c52fc1328c3bdfc7cc4989
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-10-16 17:15:39 +0200

    feat: add desktop host

--- hosts/default.nix
+++ hosts/default.nix
@@ -18,6 +18,25 @@
 
   lib = nixpkgs.lib;
 in {
+  desktop = lib.nixosSystem {
+    inherit system;
+    specialArgs = {inherit inputs user location hyprland;};
+    modules = [
+      ./desktop
+      ./configuration.nix
+
+      home-manager.nixosModules.home-manager
+      {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.extraSpecialArgs = {inherit user;};
+        home-manager.users.${user} = {
+          imports = [(import ./home.nix)] ++ [(import ./desktop/home.nix)];
+        };
+      }
+    ];
+  };
+
   laptop = lib.nixosSystem {
     inherit system;
     specialArgs = {inherit inputs user location hyprland;};
---
+++ hosts/desktop/default.nix
@@ -0,0 +1,48 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [(import ./hardware-configuration.nix)]
+    ++ [(import ../../modules/desktop/gnome)]
+    ++ (import ../../modules/hardware)
+    ++ (import ../../modules/desktop/virtualization);
+
+  networking = {
+    hostName = "desktop";
+    networkmanager.enable = true;
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Lots of these are from the default `configuration.nix`
+  boot = {
+    # Bootloader.
+    loader = {
+      systemd-boot.enable = true;
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot/efi";
+      };
+    };
+
+    initrd = {
+      # Setup keyfile
+      secrets."/crypto_keyfile.bin" = null;
+
+      # Enable swap on luks
+      luks.devices."luks-08f4eec8-fe32-472a-8e3c-4213b620923b".device = "/dev/disk/by-uuid/08f4eec8-fe32-472a-8e3c-4213b620923b";
+      luks.devices."luks-08f4eec8-fe32-472a-8e3c-4213b620923b".keyFile = "/crypto_keyfile.bin";
+    };
+  };
+}
