92825dff2ac9ccb64578a69d770162f6896bf7f9
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-10-29 15:10:29 +0200

    feat: migrate alacritty config

--- modules/programs/alacritty/default.nix
+++ modules/programs/alacritty/default.nix
@@ -1,6 +1,31 @@
 {config, ...}: {
-  home.file.".config/alacritty/alacritty.yml".source = config.lib.file.mkOutOfStoreSymlink ./alacritty.yml;
   programs.alacritty = {
     enable = true;
+    settings = {
+      window = {
+        opacity = 0.85;
+        dimensions = {
+          columns = 100;
+          lines = 25;
+        };
+        padding = {
+          x = 5;
+          y = 5;
+        };
+      };
+      font = {
+        normal = {
+          family = "JetBrainsMono";
+          style = "Regular";
+        };
+        size = 10.0;
+      };
+      # See: https://stackoverflow.com/questions/67463932/is-it-possible-to-make-alacritty-starts-with-tmux
+      shell = {
+        program = "/etc/profiles/per-user/not-matthias/bin/fish";
+        args = ["-l" "-c" "tmux attach || tmux"];
+      };
+      live_config_reload = true;
+    };
   };
 }
