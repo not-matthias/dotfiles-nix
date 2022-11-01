41f98d7cd63f49474e28f4b8dbea540515aaf78f
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-11-01 15:24:58 +0100

    feat: add onedrive config

--- modules/services/onedrive/default.nix
+++ modules/services/onedrive/default.nix
@@ -1,6 +1,16 @@
-{...}: {
-  # TODO: https://nixos.wiki/wiki/OneDrive
-  # services.onedrive = {
-  #   enable = true;
-  # };
+{pkgs, ...}: {
+  # TODO:
+  # https://github.com/KubqoA/dotfiles/blob/bc8aec4fb033740ffedf61b8580577902c156d6b/modules/services/onedrive.nix
+  # Run with:
+  # `onedrive --monitor`
+  # systemctl --user enable onedrive                                                                                                          deadnix
+  # systemctl --user start onedrive
+
+  home.packages = [
+    pkgs.onedrive
+  ];
+
+  home.file.".config/onedrive/config".text = ''
+    sync_dir = "/mnt/data/onedrive"
+  '';
 }
