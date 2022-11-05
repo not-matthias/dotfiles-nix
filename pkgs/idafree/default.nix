6ac3e1af03e7b26a0a92f9c117a6b1fce5374763
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-11-05 01:12:47 +0100

    feat: fix ida free installation

--- pkgs/idafree/default.nix
+++ pkgs/idafree/default.nix
@@ -1,56 +1,91 @@
 # https://github.com/nix-community/nur-combined/blob/master/repos/htr/pkgs/idafree/default.nix
-# Run with: nix-build -E 'with import <nixpkgs> { }; pkgs.libsForQt5.callPackage ./default.nix {}'      
+# Run with: nix-build -E 'with import <nixpkgs> { }; pkgs.libsForQt5.callPackage ./default.nix {}'
 {
-  pkgs ? import <nixpkgs> {},
+  pkgs,
   qtbase,
+  stdenv,
   wrapQtAppsHook,
   ...
 }: let
-  idafree-installer-package = pkgs.fetchurl {
+  installer = pkgs.fetchurl {
     url = "https://out7.hex-rays.com/files/idafree81_linux.run";
     sha256 = "4QRXQY94Ga0LWzV4s8gSX29D1gkvxJqoco+IyXcS8BU=";
     executable = true;
   };
-  fhsIDAWrapper = pkgs.buildFHSUserEnv rec {
-    name = "fhs-ida-wrapper";
 
+  idaWrapper = pkgs.buildFHSUserEnv rec {
+    name = "ida-wrapper";
     multiPkgs = pkgs:
       with pkgs; [
-        qtbase
-        wrapQtAppsHook
-        zlib
         libGL
+        zlib
         glib
-        gtk3
-        gtk2
-        glib
-        cairo
-        pango
-        libdrm
-        gdk-pixbuf
         freetype
         dbus
         fontconfig
+        # xorg.libXinerama
+        # xorg.libXdamage
+        # xorg.libXcursor
+        # xorg.libXrender
+        # xorg.libXScrnSaver
+        # xorg.libXxf86vm
+        # xorg.libXi
+        xorg.libSM
+        xorg.libICE
+        # xorg.libXt
+        # xorg.libXmu
+        # xorg.libXcomposite
+        # xorg.libXtst
+        # xorg.libXrandr
+        # xorg.libXext
+        xorg.libX11
+        # xorg.libXfixes
+        # xorg.xkeyboardconfig
+        xorg.libxcb
+        xorg.xcbutil
+        xorg.xcbproto
+        xorg.xcbutilwm
+        xorg.xcbutilimage
+        xorg.xcbutilerrors
+        xorg.xcbutilkeysyms
+        xorg.xcbutilrenderutil
+        libxkbcommon
       ];
-    runScript = pkgs.writeScript "fhs-ida-wrapper" ''
+    runScript = pkgs.writeScript "ida-wrapper" ''
       exec -- "$@"
     '';
   };
 in
-  pkgs.stdenv.mkDerivation {
-    name = "idafree-bin";
+  stdenv.mkDerivation {
+    pname = "ida";
+    version = "6.7.1";
     unpackPhase = "true";
+    sourceRoot = ".";
 
-    buildInputs = [qtbase];
-    nativeBuildInputs = [wrapQtAppsHook];
+    # We want to run wrapProgram manually (with additional parameters)
+    dontWrapGApps = true;
+    dontWrapQtApps = true;
+
+    nativeBuildInputs = with pkgs; [
+      wrapGAppsHook
+      wrapQtAppsHook
+    ];
+
+    # QT_QPA_PLATFORM_PLUGIN_PATH="${qt5.qtbase.bin}/lib/qt-${qt5.qtbase.version}/plugins";
+    buildInputs = [
+      qtbase
+      # pkgs.qt5.full
+    ];
     installPhase = ''
       mkdir -p $out/bin $out/opt
+
       cat <<EOF > $out/bin/idafree
       #!/bin/sh
       cd $out/opt
-      exec ${fhsIDAWrapper}/bin/fhs-ida-wrapper ./ida64
+      exec ${idaWrapper}/bin/ida-wrapper ./ida64
       EOF
       chmod +x $out/bin/idafree
-      ${fhsIDAWrapper}/bin/fhs-ida-wrapper ${idafree-installer-package} --prefix $out/opt  --mode unattended --installpassword x
+
+      ${idaWrapper}/bin/ida-wrapper ${installer} --prefix $out/opt  --mode unattended --installpassword x
     '';
   }
