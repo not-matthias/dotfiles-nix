be34085f665d8276a5d7d75caae3935e738892fd
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-11-05 20:53:25 +0100

    feat: replace launcher with prismlauncher

--- modules/games/minecraft/default.nix
+++ modules/games/minecraft/default.nix
@@ -1,6 +1,15 @@
-{pkgs, ...}: {
+{pkgs, ...}: 
+let 
+  bleachhack = builtins.fetchurl {
+    url = "https://github.com/BleachDev/BleachHack/releases/download/1.2.6/bleachhack-1.19.1.jar";
+    sha256 = "sha256:1b91aqq8lfwpw5xck67k0fl4psnw5pn0rbakg4z2ifr1c4c5kzpg";
+  };
+
+in
+{
   home.packages = with pkgs; [
-    minecraft
-    fabric-installer
+    prismlauncher
   ];
+
+  home.file.".local/share/PrismLauncher/instances/1.19.2/.minecraft/mods/bleachhack-1.19.1.jar".source = bleachhack;
 }
