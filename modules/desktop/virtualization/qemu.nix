21fe8e7a7af68aadbd5c05569111ea66cb51f88c
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-11-29 15:49:48 +0100

    feat: change qemu

--- modules/desktop/virtualization/qemu.nix
+++ modules/desktop/virtualization/qemu.nix
@@ -4,24 +4,19 @@
   ...
 }: {
   # Add libvirtd and kvm to userGroups
-  boot.extraModprobeConfig = ''
-    options kvm_amd nested=1
-  '';
-  #   boot.extraModprobeConfig = ''
-  #     options kvm_amd nested=1
-  #     options kvm_amd emulate_invalid_guest_state=0
-  #     options kvm ignore_nsrs=1
-  #   '';                                         # Needed to run OSX-KVM
-
+  # boot.extraModprobeConfig = ''
+  #   options kvm_amd nested=1
+  # '';
+  boot.extraModprobeConfig = "options kvm_intel nested=1";
   users.groups.libvirtd.members = ["root" "${user}"];
-
   virtualisation = {
     libvirtd = {
       enable = true;
       qemu = {
-        verbatimConfig = ''
-          nvram = [ "${pkgs.OVMF}/FV/OVMF.fd:${pkgs.OVMF}/FV/OVMF_VARS.fd" ]
-        '';
+        runAsRoot = false;
+        # verbatimConfig = ''
+        #   nvram = [ "${pkgs.OVMF}/FV/OVMF.fd:${pkgs.OVMF}/FV/OVMF_VARS.fd" ]
+        # '';
         package = pkgs.qemu_kvm;
       };
     };
