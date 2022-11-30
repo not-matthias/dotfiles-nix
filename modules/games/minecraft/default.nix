b759646b370c9ceb2282a7bff312bc530538e8b1
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-11-30 13:42:13 +0100

    feat: add devenv

--- modules/games/minecraft/default.nix
+++ modules/games/minecraft/default.nix
@@ -1,12 +1,9 @@
-{pkgs, ...}: 
-let 
+{pkgs, ...}: let
   bleachhack = builtins.fetchurl {
     url = "https://github.com/BleachDev/BleachHack/releases/download/1.2.6/bleachhack-1.19.1.jar";
     sha256 = "sha256:1b91aqq8lfwpw5xck67k0fl4psnw5pn0rbakg4z2ifr1c4c5kzpg";
   };
-
-in
-{
+in {
   home.packages = with pkgs; [
     prismlauncher
   ];
