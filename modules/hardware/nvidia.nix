5f1e7c96cbb6a60b9632f5492a629a2293286c29
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-12-09 23:51:59 +0100

    feat: add nvidia option

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -1,17 +1,25 @@
-{config, ...}: let
+{lib, config, ...}: let 
+  cfg = config.hardware.nvidia;
   nvidia_x11 = config.boot.kernelPackages.nvidiaPackages.legacy_470;
 in {
-  services.xserver.videoDrivers = ["nvidia"];
-  boot.blacklistedKernelModules = ["nouveau"];
-  hardware = {
-    nvidia = {
-      package = nvidia_x11;
-      modesetting.enable = true;
-      nvidiaPersistenced = true;
-    };
-    opengl = {
-      enable = true;
-      driSupport32Bit = true;
+  options.hardware.nvidia = {
+    enable = lib.mkEnableOption "Nvidia Configuration";
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.xserver.videoDrivers = ["nvidia"];
+    boot.blacklistedKernelModules = ["nouveau"];
+
+    hardware = {
+      nvidia = {
+        package = nvidia_x11;
+        modesetting.enable = true;
+        nvidiaPersistenced = true;
+      };
+      opengl = {
+        enable = true;
+        driSupport32Bit = true;
+      };
     };
   };
 }
