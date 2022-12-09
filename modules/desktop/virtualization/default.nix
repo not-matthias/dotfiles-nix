6b939369f934b829b84c52d2d039c9fc96c6fe88
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-12-09 22:33:19 +0100

    feat: add vfio (nested virt)

--- modules/desktop/virtualization/default.nix
+++ modules/desktop/virtualization/default.nix
@@ -1,4 +1,5 @@
 [
   ./docker.nix
   ./qemu.nix
+  ./vfio.nix
 ]
--- modules/desktop/virtualization/qemu.nix
+++ modules/desktop/virtualization/qemu.nix
@@ -1,13 +1,10 @@
+# References:
+# https://discourse.nixos.org/t/gpu-passthrough-shows-black-screen/17435
 {
   pkgs,
   user,
   ...
 }: {
-  # Add libvirtd and kvm to userGroups
-  # boot.extraModprobeConfig = ''
-  #   options kvm_amd nested=1
-  # '';
-  boot.extraModprobeConfig = "options kvm_intel nested=1";
   users.groups.libvirtd.members = ["root" "${user}"];
   virtualisation = {
     libvirtd = {
@@ -37,29 +34,4 @@
     # Enable file sharing between OS
     gvfs.enable = true;
   };
-
-  #boot ={
-  #  kernelParams = [ "intel_iommu=on" "vfio" "vfio_iommu_type1" "vfio_pci" "vfio_virqfd" ];      # or amd_iommu (cpu)
-  #  kernelModules = [ "vendor-reset" "vfio" "vfio_iommu_type1" "vfio_pci" "vfio_virqfd"];
-  #  extraModulePackages = [ config.boot.kernelPackages.vendor-reset ]; # Presumably fix for GPU Reset Bug
-  #  extraModprobeConfig = "options vfio-pci ids=1002:67DF,1002:AAF0"; # grep PCI_ID /sys/bus/pci/devices/*/uevent
-  #  kernelPatches = [
-  #    {
-  #    name = "vendor-reset-reqs-and-other-stuff";
-  #    patch = null;
-  #    extraConfig = ''
-  #    FTRACE y
-  #    KPROBES y
-  #    FUNCTION_TRACER y
-  #    HWLAT_TRACER y
-  #    TIMERLAT_TRACER y
-  #    IRQSOFF_TRACER y
-  #    OSNOISE_TRACER y
-  #    PCI_QUIRKS y
-  #    KALLSYMS y
-  #    KALLSYMS_ALL y
-  #    '';
-  #    }
-  #  ];
-  #};
 }
