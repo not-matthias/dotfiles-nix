52d6a404bfcfa99be1a91e89404e77888afd344e
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-12-29 14:44:58 +0100

    chore: upgrade flaek

--- modules/home/programs/spotify/default.nix
+++ modules/home/programs/spotify/default.nix
@@ -7,22 +7,23 @@
 in {
   programs.spicetify = {
     enable = true;
-    theme = "BurntSienna";
+    theme = spicePkgs.themes.BurntSienna;
     enabledCustomApps = with spicePkgs.apps; [
       new-releases
       reddit
       marketplace
     ];
-    enabledExtensions = [
-      "keyboardShortcut.js"
-      "fullAppDisplay.js"
-      "shuffle+.js"
-      "hidePodcasts.js"
-      "songStats.js"
-      "autoVolume.js"
-      "history.js"
-      "genre.js"
-      "adblock.js"
+    enabledExtensions = with spicePkgs.extensions; [
+      keyboardShortcut
+      history
+      fullAppDisplay
+      shuffle
+      hidePodcasts
+      songStats
+      autoVolume
+      history
+      genre
+      adblock
     ];
   };
 }
