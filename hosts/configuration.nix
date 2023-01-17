6d66abdd3e6a16c6ddce7ada45190ecb6b9479f5
not-matthias <26800596+not-matthias@users.noreply.github.com>
2023-01-17 20:03:01 +0100

    chore: code cleanup

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -20,17 +20,6 @@
   time.timeZone = "Europe/Vienna";
   i18n = {
     defaultLocale = "en_US.utf8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "de_AT.utf8";
-      LC_IDENTIFICATION = "de_AT.utf8";
-      LC_MEASUREMENT = "de_AT.utf8";
-      LC_MONETARY = "de_AT.utf8";
-      LC_NAME = "de_AT.utf8";
-      LC_NUMERIC = "de_AT.utf8";
-      LC_PAPER = "de_AT.utf8";
-      LC_TELEPHONE = "de_AT.utf8";
-      LC_TIME = "de_AT.utf8";
-    };
   };
 
   console = {
