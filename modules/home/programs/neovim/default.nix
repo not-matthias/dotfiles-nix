06090cc662ec180903ef8420a148aa9df503dfad
not-matthias <26800596+not-matthias@users.noreply.github.com>
2023-01-28 17:16:44 +0100

    feat: add more plugins

--- modules/home/programs/neovim/default.nix
+++ modules/home/programs/neovim/default.nix
@@ -1,4 +1,21 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  # https://github.com/azuwis/nix-config/blob/885e77f74bd730f37d715c6a7ed1a9269a619f7d/common/neovim/nvchad.nix
+  nvchad = pkgs.vimUtils.buildVimPluginFrom2Nix {
+    pname = "nvchad";
+    version = "unstable-2022-12-24";
+    src = pkgs.fetchFromGitHub {
+      owner = "NvChad";
+      repo = "NvChad";
+      rev = "e6a230129ac16f23120424f4cee35fedcb6bf774";
+      sha256 = "1kamjv7gnc7jbhi10cnpgpa2m3xw8a9j8yvgadcqfcw9kigiw5v4";
+    };
+    meta.homepage = "https://github.com/NvChad/NvChad/";
+  };
+in {
+  home.packages = with pkgs; [
+    neovide
+  ];
+
   # https://github.com/tars0x9752/home/blob/main/modules/neovim/default.nix
   # https://github.com/notusknot/dotfiles-nix/blob/main/modules/nvim/default.nix
   programs.neovim = {
@@ -7,14 +24,49 @@
     extraConfig =
       builtins.readFile ./init.vim;
     plugins = with pkgs.vimPlugins; [
-      #      vim-airline
-      #      vim-gitgutter
-      #      vim-ormolu
+      nvchad
+
+      vim-airline
+      vim-gitgutter
+      vim-ormolu
       vim-nix
       vim-autoformat
-      #      supertab
+      vim-nix
+      vim-nixhash
+      vim-yaml
+      vim-toml
+
       fzf-vim
       nerdtree
+
+      {
+        plugin = nvim-treesitter;
+        config = ''
+          lua << EOF
+          require('nvim-treesitter.configs').setup {
+              highlight = {
+                  enable = true,
+                  additional_vim_regex_highlighting = false,
+              },
+          }
+          EOF
+        '';
+      }
+      {
+        plugin = nvim-lspconfig;
+        config = ''
+          lua << EOF
+          require('lspconfig').rust_analyzer.setup{}
+          require('lspconfig').rnix.setup{}
+          EOF
+        '';
+      }
+      {
+        plugin = telescope-nvim;
+        config = "lua require('telescope').setup()";
+      }
+
+      telescope-zoxide
 
       # coc
       coc-json
@@ -37,4 +89,8 @@
       };
     };
   };
+
+  xdg.configFile."nvim/init.lua".text = ''
+    vim.cmd [[source ${nvchad}/init.lua]]
+  '';
 }
