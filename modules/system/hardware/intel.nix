95c4d5ceab3411056cf86b6df368fdcbbd082cd9
not-matthias <26800596+not-matthias@users.noreply.github.com>
2023-02-22 15:36:44 +0100

    feat: update intel gpu

--- modules/system/hardware/intel.nix
+++ modules/system/hardware/intel.nix
@@ -11,19 +11,18 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    nixpkgs.config.packageOverrides = pkgs: {
-      vaapiIntel = pkgs.vaapiIntel.override {enableHybridCodec = true;};
-    };
     hardware.opengl = {
       enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
       extraPackages = with pkgs; [
-        mesa_drivers
         intel-media-driver
         vaapiIntel
         vaapiVdpau
         libvdpau-va-gl
       ];
     };
+    hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [vaapiIntel];
 
     services.xserver.videoDrivers = ["intel"];
   };
