0eb62132770f231dc0dadb88d3a81332ddaa2ec0
not-matthias <26800596+not-matthias@users.noreply.github.com>
2023-05-12 17:59:33 +0200

    chore: restructure services

--- modules/home/services/default.nix
+++ modules/home/services/default.nix
@@ -2,5 +2,4 @@
   # ./flameshot # Replaced with the gnome screenshot utility
   # ./redshift # Replaced with gnome nightlight
   ./onedrive
-  ./fprintd
 ]
--- modules/system/programs/default.nix
+++ modules/system/programs/default.nix
@@ -1,5 +1,4 @@
 [
-  ./flatpak
   ./noisetorch
   ./nix-ld
   ./steam
