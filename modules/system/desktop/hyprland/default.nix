d767b85f1ed00e278b271176ea0a0fc38de34aad
not-matthias <26800596+not-matthias@users.noreply.github.com>
2023-08-07 20:53:22 +0200

    chore: minor changes
    - Add brightness, audio and media controls
    - Switch to nautilus

--- modules/system/desktop/hyprland/default.nix
+++ modules/system/desktop/hyprland/default.nix
@@ -39,7 +39,7 @@
 
   environment.systemPackages = with pkgs; [
     kitty
-    dolphin
+    gnome.nautilus
 
     swaylock
     wl-clipboard
--- modules/system/desktop/hyprland/home/default.nix
+++ modules/system/desktop/hyprland/home/default.nix
@@ -1,11 +1,18 @@
 {user, ...}: {
-  home-manager.users.${user} = {...}: {
+  home-manager.users.${user} = {pkgs, ...}: {
     imports = [
       ./swayidle.nix
       ./swayidle.nix
       ./swww.nix
     ];
 
+    home.packages = with pkgs; [
+      pamixer # for volume control
+      brightnessctl # for brightness control
+      playerctl # for media control
+    ];
+
+    home.file.".config/hypr/env.conf".text = builtins.readFile ./conf/env.conf;
     home.file.".config/hypr/hyprland.conf".text = builtins.readFile ./conf/hyprland.conf;
     home.file.".config/hypr/keybinds.conf".text = builtins.readFile ./conf/keybinds.conf;
     home.file.".config/hypr/startup.conf".text = builtins.readFile ./conf/startup.conf;
