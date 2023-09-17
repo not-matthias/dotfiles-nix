1f43a0f144f091d56c03f1f984101c50f0bdce3f
not-matthias <26800596+not-matthias@users.noreply.github.com>
2023-09-17 15:24:27 +0200

    fix: ignore player status when suspending
    When both my laptop and phone are connected to my bluetooth headphones, the laptop will never suspend.

--- modules/system/desktop/hyprland/home/swayidle.nix
+++ modules/system/desktop/hyprland/home/swayidle.nix
@@ -1,10 +1,13 @@
 {pkgs, ...}: let
+  # suspendScript = pkgs.writeShellScript "suspend-script" ''
+  #   ${pkgs.pipewire}/bin/pw-cli i all | ${pkgs.ripgrep}/bin/rg running
+  #   # only suspend if audio isn't running
+  #   if [ $? == 1 ]; then
+  #     ${pkgs.systemd}/bin/systemctl suspend
+  #   fi
+  # '';
   suspendScript = pkgs.writeShellScript "suspend-script" ''
-    ${pkgs.pipewire}/bin/pw-cli i all | ${pkgs.ripgrep}/bin/rg running
-    # only suspend if audio isn't running
-    if [ $? == 1 ]; then
-      ${pkgs.systemd}/bin/systemctl suspend
-    fi
+    ${pkgs.systemd}/bin/systemctl suspend
   '';
 in {
   # screen idle
