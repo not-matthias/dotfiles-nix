8f2c3862bdd3a4757b40f50ba72335921219e40c
not-matthias <26800596+not-matthias@users.noreply.github.com>
2024-08-07 06:03:53 +0200

    chore: disable tmux in alacritty

--- modules/home/programs/alacritty/default.nix
+++ modules/home/programs/alacritty/default.nix
@@ -19,11 +19,13 @@
         };
         size = 10.0;
       };
+
       # See: https://stackoverflow.com/questions/67463932/is-it-possible-to-make-alacritty-starts-with-tmux
-      shell = {
-        program = "/etc/profiles/per-user/not-matthias/bin/fish";
-        args = ["-l" "-c" "tmux attach || tmux"];
-      };
+      # shell = {
+      #   program = "/etc/profiles/per-user/not-matthias/bin/fish";
+      #   args = ["-l" "-c" "tmux attach || tmux"];
+      # };
+
       live_config_reload = true;
 
       # Black background
