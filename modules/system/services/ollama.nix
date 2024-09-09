c7b9cc18fb93988012e137be2046a3b1524d08b9
not-matthias <26800596+not-matthias@users.noreply.github.com>
2024-09-09 14:13:31 +0200

    chore: use stable open-webui

--- modules/system/services/ollama.nix
+++ modules/system/services/ollama.nix
@@ -1,6 +1,7 @@
 {
   unstable,
   config,
+  pkgs,
   ...
 }: {
   networking.firewall.allowedTCPPorts = [
@@ -18,23 +19,18 @@
 
   services.open-webui = {
     enable = true;
-    package = unstable.open-webui;
+    package = pkgs.open-webui;
     host = "0.0.0.0";
     port = 11435;
     environment = {
       OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
 
-      # SCARF_NO_ANALYTICS = "True";
-      # DO_NOT_TRACK = "True";
-      # ANONYMIZED_TELEMETRY = "False";
+      SCARF_NO_ANALYTICS = "True";
+      DO_NOT_TRACK = "True";
+      ANONYMIZED_TELEMETRY = "False";
 
       #ENABLE_COMMUNITY_SHARING = "False";
       #ENABLE_ADMIN_EXPORT = "False";
-
-      #WEBUI_AUTH = "False";
-      #ENABLE_SIGNUP = "False";
-      #WEBUI_AUTH_TRUSTED_EMAIL_HEADER = "X-Email";
-      #DEFAULT_USER_ROLE = "user";
     };
   };
 }
