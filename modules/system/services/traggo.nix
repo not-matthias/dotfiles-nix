d39e08c1be5995885c21adc890ca1120a7cbf50c
not-matthias <26800596+not-matthias@users.noreply.github.com>
2024-11-23 15:42:32 +0100

    fix: disable traggo by deafult

--- modules/system/services/traggo.nix
+++ modules/system/services/traggo.nix
@@ -1,19 +1,30 @@
 {
-  virtualisation.arion.backend = "docker";
+  lib,
+  config,
+}: let
+  cfg = config.services.traggo;
+in {
+  options.services.traggo = {
+    enable = lib.mkEnableOption "Enable Traggo";
+  };
 
-  virtualisation.arion.projects.traggo.settings.services = {
-    traggo.service = {
-      image = "traggo/server:latest";
-      environment = {
-        "TRAGGO_DEFAULT_USER_NAME" = "admin";
-        "TRAGGO_DEFAULT_USER_PASS" = "admin";
+  config = lib.mkIf cfg.enable {
+    virtualisation.arion.backend = "docker";
+
+    virtualisation.arion.projects.traggo.settings.services = {
+      traggo.service = {
+        image = "traggo/server:latest";
+        environment = {
+          "TRAGGO_DEFAULT_USER_NAME" = "admin";
+          "TRAGGO_DEFAULT_USER_PASS" = "admin";
+        };
+        volumes = [
+          "/var/lib/traggo/data:/opt/traggo/data:rw"
+        ];
+        ports = [
+          "3030:3030/tcp"
+        ];
       };
-      volumes = [
-        "/var/lib/traggo/data:/opt/traggo/data:rw"
-      ];
-      ports = [
-        "3030:3030/tcp"
-      ];
     };
   };
 }
