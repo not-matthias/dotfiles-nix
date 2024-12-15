c04c212e90707212f77cff9870f9e5ec805aa0ab
not-matthias <26800596+not-matthias@users.noreply.github.com>
2024-12-15 18:19:32 +0100

    chore: don't include hyprland home if hyprland is not set

--- modules/system/desktop/hyprland/home/default.nix
+++ modules/system/desktop/hyprland/home/default.nix
@@ -1,35 +1,46 @@
-{user, ...}: {
-  home-manager.users.${user} = {pkgs, ...}: {
-    imports = [
-      ./swayidle.nix
-      ./swayidle.nix
-      ./swww.nix
-      ./wlsunset.nix
-    ];
+{
+  user,
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.desktop.hyprland;
+in {
+  config = lib.mkIf cfg.enable {
+    home-manager.users.${user} = {pkgs, ...}: {
+      imports = [
+        ./swayidle.nix
+        ./swayidle.nix
+        ./swww.nix
+        ./wlsunset.nix
+      ];
 
-    home.packages = with pkgs; [
-      alsa-utils # for volume control
-      brightnessctl # for brightness control
-      playerctl # for media control
-      networkmanagerapplet # for network control
+      home.packages = with pkgs; [
+        alsa-utils # for volume control
+        brightnessctl # for brightness control
+        playerctl # for media control
+        networkmanagerapplet # for network control
 
-      wofi-emoji
-      wtype
+        wofi-emoji
+        wtype
 
-      # screenshot tools
-      grim
-      slurp
-      swappy
-      tesseract
-    ];
+        # screenshot tools
+        grim
+        slurp
+        swappy
+        tesseract
 
-    home.file.".config/hypr/env.conf".text = builtins.readFile ./conf/env.conf;
-    home.file.".config/hypr/hyprland.conf".text =
-      builtins.readFile ./conf/hyprland.conf
-      + "\n"
-      + builtins.readFile ./conf/fcitx5.conf;
-    home.file.".config/hypr/keybinds.conf".text = builtins.readFile ./conf/keybinds.conf;
-    home.file.".config/hypr/startup.conf".text = builtins.readFile ./conf/startup.conf;
-    home.file.".config/hypr/windowrule.conf".text = builtins.readFile ./conf/windowrule.conf;
+        jetbrains.rust-rover
+      ];
+
+      home.file.".config/hypr/env.conf".text = builtins.readFile ./conf/env.conf;
+      home.file.".config/hypr/hyprland.conf".text =
+        builtins.readFile ./conf/hyprland.conf
+        + "\n"
+        + builtins.readFile ./conf/fcitx5.conf;
+      home.file.".config/hypr/keybinds.conf".text = builtins.readFile ./conf/keybinds.conf;
+      home.file.".config/hypr/startup.conf".text = builtins.readFile ./conf/startup.conf;
+      home.file.".config/hypr/windowrule.conf".text = builtins.readFile ./conf/windowrule.conf;
+    };
   };
 }
