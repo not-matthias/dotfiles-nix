6cd9305f5527da7f93ff7c16193da04c0a8e1d2f
not-matthias <26800596+not-matthias@users.noreply.github.com>
2025-02-05 21:06:04 +0100

    chore: switch to auto-cpufreq

--- modules/system/hardware/powersave.nix
+++ modules/system/hardware/powersave.nix
@@ -24,19 +24,21 @@ in {
 
     boot = {
       # snd_hda_intel: Audio Power saving
+      # iwlwifi: https://wiki.archlinux.org/title/Power_management#Intel_wireless_cards_(iwlwifi)
       extraModprobeConfig = ''
         options snd_hda_intel power_save=1
-
         options iwlwifi power_save=1 d0i3_disable=0 uapsd_disable=0
         options iwlmvm power_scheme=3
       '';
 
       kernel.sysctl = {
         # Disable Watchdog, can lead to significant power savings
+        # See: https://wiki.archlinux.org/title/Power_management#Disabling_NMI_watchdog
         "kernel.nmi_watchdog" = 0;
 
         # https://lonesysadmin.net/2013/12/22/better-linux-disk-caching-performance-vm-dirty_ratio/
         # https://askubuntu.com/questions/847703/how-to-change-the-value-of-dirty-writeback-centisecs
+        # https://wiki.archlinux.org/title/Power_management#Writeback_Time
         "vm.dirty_writeback_centisecs" = 1500;
 
         # Swap to disk less
@@ -47,40 +49,18 @@ in {
       };
     };
 
-    # See:
-    # https://knowledgebase.frame.work/optimizing-ubuntu-battery-life-Sye_48Lg3
-    # https://github.com/nzbr/nixos/blob/cd219d6e0bd968f749cd74a9289800b9e67775c0/module/pattern/laptop.nix#L13-L22
-    # https://github.com/krebs/stockholm/blob/40f103e9ccf99dc36c92e2e008ed7a0b3dca1f48/makefu/2configs/hw/tp-x2x0.nix#L41-L50
-    # - https://wiki.archlinux.org/title/CPU_frequency_scaling
-    #
+    # Prevent overheating on Intel cpus
     services.thermald.enable = true;
-    services.power-profiles-daemon.enable = false;
-    services.tlp = {
-      enable = true;
-      settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BATTERY = "powersave";
-
-        START_CHARGE_THRESH_BAT0 = 90;
-        STOP_CHARGE_THRESH_BAT0 = 97;
-        RUNTIME_PM_ON_BAT = "auto";
-
-        # tlp-stat -g
-        # INTEL_GPU_MIN_FREQ_ON_AC = 500;
-        # INTEL_GPU_MIN_FREQ_ON_BAT = 500;
-      };
-    };
 
     services.auto-cpufreq = {
-      enable = false;
-      # https://github.com/AdnanHodzic/auto-cpufreq/blob/master/auto-cpufreq.conf-example
+      enable = true;
       settings = {
-        charger = {
-          governor = "performance";
-          turbo = "auto";
-        };
         battery = {
           governor = "powersave";
+          turbo = "never";
+        };
+        charger = {
+          governor = "performance";
           turbo = "auto";
         };
       };
@@ -90,6 +70,40 @@ in {
       enable = true;
       powertop.enable = true;
       cpuFreqGovernor = lib.mkDefault "powersave";
+    };
+
+    # See:
+    # https://knowledgebase.frame.work/optimizing-ubuntu-battery-life-Sye_48Lg3
+    # https://github.com/nzbr/nixos/blob/cd219d6e0bd968f749cd74a9289800b9e67775c0/module/pattern/laptop.nix#L13-L22
+    # https://github.com/krebs/stockholm/blob/40f103e9ccf99dc36c92e2e008ed7a0b3dca1f48/makefu/2configs/hw/tp-x2x0.nix#L41-L50
+    # - https://wiki.archlinux.org/title/CPU_frequency_scaling
+    #
+    services.power-profiles-daemon.enable = false;
+    services.tlp = {
+      enable = false;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BATTERY = "powersave";
+
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 20;
+
+        # Save long term battery health
+        START_CHARGE_THRESH_BAT0 = 40;
+        STOP_CHARGE_THRESH_BAT0 = 80;
+
+        RUNTIME_PM_ON_BAT = "auto";
+
+        # tlp-stat -g
+        # INTEL_GPU_MIN_FREQ_ON_AC = 500;
+        # INTEL_GPU_MIN_FREQ_ON_BAT = 500;
+        #
+      };
     };
   };
 }
