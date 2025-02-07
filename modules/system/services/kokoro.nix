f8a5198ddb6a9564493061ec8f6d2e5f976b5ce5
not-matthias <26800596+not-matthias@users.noreply.github.com>
2025-02-07 22:44:57 +0100

    chore: add kokoro options

--- modules/system/services/kokoro.nix
+++ modules/system/services/kokoro.nix
@@ -7,35 +7,57 @@
 in {
   options.services.kokoro = {
     enable = lib.mkEnableOption "Kokoro TTS";
+    useGpu = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Use GPU for Kokoro TTS";
+    };
+    ui-port = lib.mkOption {
+      type = lib.types.int;
+      default = 7860;
+      description = "Port for the Kokoro UI";
+    };
+    api-port = lib.mkOption {
+      type = lib.types.int;
+      default = 8880;
+      description = "Port for the Kokoro API";
+    };
+    version = lib.mkOption {
+      type = lib.types.str;
+      default = "v0.2.0";
+      description = "Version of the Kokoro TTS";
+    };
   };
 
   config = lib.mkIf cfg.enable {
-    virtualisation = {
-      docker.enable = true;
-      oci-containers.containers = let
-        kokoro_version = "v0.1.0";
-        kokoro_api_port = 8880;
-        kokoro_ui_port = 7860;
-      in {
-        kokoro = {
-          image = "ghcr.io/remsky/kokoro-fastapi-gpu:${kokoro_version}";
-          ports = ["${toString kokoro_api_port}:8880"];
-          volumes = ["/var/lib/kokoro/voices:/app/api/src/voices"];
-          devices = ["nvidia.com/gpu=0"];
-          environment = {
-            PYTHONPATH = "/app:/app/models";
-          };
+    virtualisation.oci-containers.containers = let
+      image-name =
+        if cfg.useGpu
+        then "ghcr.io/remsky/kokoro-fastapi-gpu:${cfg.version}"
+        else "ghcr.io/remsky/kokoro-fastapi-cpu:${cfg.version}";
+    in {
+      kokoro = {
+        image = "${image-name}";
+        ports = ["${toString cfg.api-port}:8880"];
+        volumes = ["/var/lib/kokoro/voices:/app/api/src/voices"];
+        environment = {
+          PYTHONPATH = "/app:/app/models";
         };
-        kokoro-ui = {
-          image = "ghcr.io/remsky/kokoro-fastapi-ui:${kokoro_version}";
-          ports = ["${toString kokoro_ui_port}:7860"];
-          volumes = ["/var/lib/kokoro/data:/app/ui/data"];
-          environment = {
-            PYTHONUNBUFFERED = "1";
-            DISABLE_LOCAL_SAVING = "false";
-          };
-          extraOptions = ["--add-host=kokoro-tts:10.88.0.1"];
+        extraOptions =
+          if cfg.useGpu
+          then ["--device=nvidia.com/gpu=all"]
+          else []; # "--gpus=all"
+      };
+
+      kokoro-ui = {
+        image = "ghcr.io/remsky/kokoro-fastapi-ui:v0.1.0";
+        ports = ["${toString cfg.ui-port}:7860"];
+        volumes = ["/var/lib/kokoro/data:/app/ui/data"];
+        environment = {
+          PYTHONUNBUFFERED = "1";
+          DISABLE_LOCAL_SAVING = "false";
         };
+        extraOptions = ["--add-host=kokoro-tts:10.88.0.1"];
       };
     };
   };
