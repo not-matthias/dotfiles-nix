36bfe6d8d45f7835687352c52e7621c63718c34f
not-matthias <26800596+not-matthias@users.noreply.github.com>
2025-06-06 21:38:34 +0200

    chore: don't store secrets and failed commands; use vim insert keymode

--- modules/home/programs/atuin.nix
+++ modules/home/programs/atuin.nix
@@ -4,9 +4,10 @@
     package = unstable.atuin;
     enableFishIntegration = true;
     settings = {
-      # fuzzy: doesn't quite work for me. example: search for env var i set before
-      search_mode = "skim";
-      # TODO: sync_address
+      search_mode = "skim"; # fuzzy: doesn't quite work for me. example: search for env var i set before
+      keymap_mode = "vim-insert";
+      secrets_filter = true;
+      store_failed = false;
     };
   };
 }
