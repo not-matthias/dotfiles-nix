21bbbedd87c8ddfe769fe7e33b4c7f8435e65917
not-matthias <26800596+not-matthias@users.noreply.github.com>
2025-06-24 19:44:03 +0200

    feat: add karakeep ai configuration

--- modules/system/services/karakeep.nix
+++ modules/system/services/karakeep.nix
@@ -10,11 +10,21 @@ in {
   config = lib.mkIf cfg.enable {
     services.karakeep = {
       package = unstable.karakeep;
-      extraEnvironment = {
+      extraEnvironment = rec {
         PORT = "9999";
 
         # TODO: Setup AI tagging
         # https://docs.karakeep.app/configuration/#inference-configs-for-automatic-tagging
+        OLLAMA_BASE_URL = "http://127.0.0.1:11434";
+        OLLAMA_KEEP_ALIVE = "5m";
+
+        INFERENCE_EMBEDDING_MODEL = "nomic-embed-text";
+        INFERENCE_IMAGE_MODEL = "gemma3:4b-it-qat";
+        INFERENCE_TEXT_MODEL = INFERENCE_IMAGE_MODEL;
+        INFERENCE_JOB_TIMEOUT_SEC = "600";
+        INFERENCE_ENABLE_AUTO_SUMMARIZATION = "false";
+        INFERENCE_ENABLE_AUTO_TAGGING = "true";
+        INFERENCE_LANG = "english";
 
         CRAWLER_VIDEO_DOWNLOAD = "true";
         CRAWLER_VIDEO_DOWNLOAD_MAX_SIZE = "-1";
