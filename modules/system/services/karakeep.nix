7034d1b73a39706ca13cf194f9cfcc44df9c75f8
not-matthias <26800596+not-matthias@users.noreply.github.com>
2025-06-27 23:37:06 +0200

    chore: update karakeep (fullpage archive, llm model)

--- modules/system/services/karakeep.nix
+++ modules/system/services/karakeep.nix
@@ -18,9 +18,13 @@ in {
         OLLAMA_BASE_URL = "http://127.0.0.1:11434";
         OLLAMA_KEEP_ALIVE = "5m";
 
+        CRAWLER_FULL_PAGE_SCREENSHOT = "true";
+        CRAWLER_FULL_PAGE_ARCHIVE = "true";
+
+        INFERENCE_CONTEXT_LENGTH = "8192";
         INFERENCE_EMBEDDING_MODEL = "nomic-embed-text";
-        INFERENCE_IMAGE_MODEL = "qwen3:30b-a3b";
-        INFERENCE_TEXT_MODEL = "gemma3:4b-it-qat";
+        INFERENCE_IMAGE_MODEL = "gemma3:4b-it-qat";
+        INFERENCE_TEXT_MODEL = "qwen3:30b-a3b";
         INFERENCE_JOB_TIMEOUT_SEC = "600";
         INFERENCE_ENABLE_AUTO_SUMMARIZATION = "false";
         INFERENCE_ENABLE_AUTO_TAGGING = "true";
