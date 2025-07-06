960af1b751b2522ca2ab67d2fb9d86124c07a974
not-matthias <26800596+not-matthias@users.noreply.github.com>
2025-07-06 14:35:13 +0200

    fix: prevent idle when music is playing

--- modules/system/desktop/hyprland/home/swayidle.nix
+++ modules/system/desktop/hyprland/home/swayidle.nix
@@ -1,5 +1,10 @@
 {pkgs, ...}: let
   suspendScript = pkgs.writeShellScript "suspend-script" ''
+    #!${pkgs.runtimeShell}
+    # Don't suspend if music is playing
+    if ${pkgs.playerctl}/bin/playerctl status 2>/dev/null | grep -q "Playing"; then
+        exit 0
+    fi
     ${pkgs.systemd}/bin/systemctl suspend
   '';
 in {
