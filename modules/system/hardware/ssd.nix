c839ca84af396fd9210778b9b7f7df29caca7cbe
not-matthias <26800596+not-matthias@users.noreply.github.com>
2025-07-30 00:07:54 +0200

    feat: trim daily

--- modules/system/hardware/ssd.nix
+++ modules/system/hardware/ssd.nix
@@ -12,6 +12,9 @@ in {
   config = lib.mkIf cfg.enable {
     # https://wiki.archlinux.org/title/Solid_state_drive#TRIM
     # - SSDs benefit from informing the disk controller when blocks of memory are free to be reused
-    services.fstrim.enable = true;
+    services.fstrim = {
+      enable = true;
+      interval = "daily";
+    };
   };
 }
