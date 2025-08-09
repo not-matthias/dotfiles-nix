64d93d8e91871d86b4f71f9cfb8bc1683d958cd6
not-matthias <26800596+not-matthias@users.noreply.github.com>
2025-08-09 16:13:53 +0200

    fix: inference text name

--- modules/system/services/karakeep.nix
+++ modules/system/services/karakeep.nix
@@ -24,7 +24,7 @@ in {
         INFERENCE_CONTEXT_LENGTH = "8192";
         INFERENCE_EMBEDDING_MODEL = "nomic-embed-text";
         INFERENCE_IMAGE_MODEL = "gemma3n:e4b";
-        INFERENCE_TEXT_MODEL = "germma3n:e4b";
+        INFERENCE_TEXT_MODEL = "gemma3n:e4b";
         INFERENCE_JOB_TIMEOUT_SEC = "600";
         INFERENCE_ENABLE_AUTO_SUMMARIZATION = "false";
         INFERENCE_ENABLE_AUTO_TAGGING = "true";
