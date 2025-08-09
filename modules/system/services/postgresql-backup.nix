4b4311f5192399057e0a28b9ea3914f91702ee09
not-matthias <26800596+not-matthias@users.noreply.github.com>
2025-08-09 17:01:34 +0200

    feat: add postgresql backup

--- modules/system/services/postgresql-backup.nix
+++ modules/system/services/postgresql-backup.nix
@@ -1,11 +1,17 @@
 {
-  services.postgresqlBackup = {
-    enable = true;
-    backupAll = true;
-    location = "/var/backup/postgresql";
-  };
+  config,
+  lib,
+  ...
+}: {
+  config = lib.mkIf config.services.postgresql.enable {
+    services.postgresqlBackup = {
+      enable = true;
+      backupAll = true;
+      location = "/var/backup/postgresql";
+    };
 
-  services.restic.paths = [
-    "/var/backup/postgresql"
-  ];
+    services.restic.paths = [
+      "/var/backup/postgresql"
+    ];
+  };
 }
