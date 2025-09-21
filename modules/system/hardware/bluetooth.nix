e9b58c35839d7a54a10d61ed6025c1d17b72d31f
not-matthias <26800596+not-matthias@users.noreply.github.com>
2025-09-21 14:52:14 +0200

    feat: update bluetooth settings

--- modules/system/hardware/bluetooth.nix
+++ modules/system/hardware/bluetooth.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: let
   cfg = config.hardware.bluetooth;
@@ -9,11 +10,24 @@ in {
     services.blueman.enable = true;
 
     hardware.bluetooth = {
-      # enable = true; # Will be set by the user
-      # hsphfpd.enable = true; # HSP & HFP daemon
+      package = pkgs.bluez-experimental;
       settings = {
         General = {
           Enable = "Source,Sink,Media,Socket";
+          Experimental = true;
+          KernelExperimental = true;
+          # When enabled other devices can connect faster to us, however
+          # the tradeoff is increased power consumption. Defaults to
+          FastConnectable = true;
+        };
+        Policy = {
+          # Enable all controllers when they are found. This includes
+          # adapters present on start as well as adapters that are plugged
+          # in later on. Defaults to 'true'.
+          AutoEnable = false;
+        };
+        LE = {
+          EnableAdvMonInterleave = true;
         };
       };
     };
