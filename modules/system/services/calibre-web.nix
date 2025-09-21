8d75387b93ada711ef37e3919d147960cad73395
not-matthias <26800596+not-matthias@users.noreply.github.com>
2025-09-21 14:51:16 +0200

    fix: calibre-web permission error

--- modules/system/services/calibre-web.nix
+++ modules/system/services/calibre-web.nix
@@ -13,6 +13,12 @@ in {
       enableBookConversion = true;
     };
 
+    # Ensure proper permissions for the Calibre library
+    systemd.tmpfiles.rules = [
+      "Z /mnt/data/personal/books/calibre-library 0775 calibre-web calibre-web -"
+      "Z /mnt/data/personal/books/calibre-library/metadata.db 0664 calibre-web calibre-web -"
+    ];
+
     services.caddy.virtualHosts."books.${domain}".extraConfig = ''
       encode zstd gzip
       reverse_proxy http://[::1]:8083
