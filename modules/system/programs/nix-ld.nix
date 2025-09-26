c7de8c2ef3fe880affcd82d85fc39f21729f37fb
not-matthias <26800596+not-matthias@users.noreply.github.com>
2025-09-26 22:39:54 +0200

    fix: remove nix-ld libraries (not needed afaik)

--- modules/system/programs/nix-ld.nix
+++ modules/system/programs/nix-ld.nix
@@ -1,5 +1,4 @@
 {
-  pkgs,
   config,
   lib,
   ...
@@ -7,30 +6,6 @@
   cfg = config.programs.nix-ld;
 in {
   config = lib.mkIf cfg.enable {
-    # programs.nix-ld.enable = true;
-    programs.nix-ld.libraries = with pkgs; [
-      acl
-      attr
-      bzip2
-      dbus
-      expat
-      fontconfig
-      freetype
-      fuse3
-      icu
-      libnotify
-      libsodium
-      libssh
-      libunwind
-      libusb1
-      libuuid
-      nspr
-      nss
-      stdenv.cc.cc
-      util-linux
-      zlib
-      zstd
-      zlib-ng
-    ];
+    # Placeholder in case we want to configure the nix-ld libraries here
   };
 }
