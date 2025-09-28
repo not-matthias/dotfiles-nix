80e7b2a8dce1cd8d644caacb510f5f3100d20f30
not-matthias <26800596+not-matthias@users.noreply.github.com>
2025-09-28 15:43:05 +0200

    fix: only include service on homepage if enabled

--- modules/system/services/homepage.nix
+++ modules/system/services/homepage.nix
@@ -1,205 +1,210 @@
-{domain, ...}: let
+{
+  domain,
+  config,
+  lib,
+  ...
+}: let
   desktopDomain = "desktopnm.duckdns.org";
+  cfg = config.services.homepage-dashboard;
+
+  # Helper function to conditionally include a service
+  includeService = serviceName: serviceConfig:
+    lib.optionals (config.services.${serviceName}.enable or false) [serviceConfig];
 in {
   # Find the icons here:
   # - https://github.com/walkxcode/Dashboard-Icons/tree/main/svg
   # - https://gethomepage.dev/configs/services/#icons
-  services.homepage-dashboard = {
-    listenPort = 7676;
-    allowedHosts = "*";
 
-    settings = {
-      title = "Homepage";
-      headerStyle = "clean";
-      statusStyle = "dot";
-      cardBlur = "sm";
-      language = "en";
-      hideVersion = true;
-      showStats = true;
-      fiveColumns = true;
+  config = lib.mkIf cfg.enable {
+    services.homepage-dashboard = {
+      listenPort = 7676;
+      allowedHosts = "*";
 
-      layout = [
+      settings = {
+        title = "Homepage";
+        headerStyle = "clean";
+        statusStyle = "dot";
+        cardBlur = "sm";
+        language = "en";
+        hideVersion = true;
+        showStats = true;
+        fiveColumns = true;
+
+        layout = [
+          {
+            "Services" = {
+              style = "row";
+              columns = 4;
+              header = false;
+            };
+          }
+        ];
+      };
+
+      services = [
         {
-          "Services" = {
-            style = "row";
-            columns = 4;
-            header = false;
+          "Services" = lib.flatten [
+            (includeService "adguardhome" {
+              "AdGuard Home" = {
+                description = "AdGuard Home";
+                icon = "adguard-home";
+                href = "https://adguard.${desktopDomain}";
+                siteMonitor = "https://adguard.${desktopDomain}";
+              };
+            })
+            (includeService "open-webui" {
+              "OpenWebUI (server)" = {
+                description = "OpenWebUI";
+                icon = "ollama";
+                href = "https://ollama.${desktopDomain}";
+                siteMonitor = "https://ollama.${desktopDomain}";
+              };
+            })
+            (includeService "miniflux" {
+              Miniflux = {
+                description = "Miniflux";
+                icon = "miniflux";
+                href = "https://rss.${desktopDomain}";
+                siteMonitor = "https://rss.${desktopDomain}";
+              };
+            })
+            (includeService "scrutiny" {
+              Scrutiny = {
+                description = "S.M.A.R.T. Monitoring";
+                icon = "scrutiny";
+                href = "https://scrutiny.${desktopDomain}";
+                siteMonitor = "https://scrutiny.${desktopDomain}";
+              };
+            })
+            (includeService "memos" {
+              Memos = {
+                description = "Memos";
+                icon = "memos";
+                href = "https://memos.${desktopDomain}";
+                siteMonitor = "https://memos.${desktopDomain}";
+              };
+            })
+            (includeService "netdata" {
+              Netdata = {
+                description = "Netdata";
+                icon = "netdata";
+                href = "https://netdata.${desktopDomain}";
+                siteMonitor = "https://netdata.${desktopDomain}";
+              };
+            })
+            (includeService "immich" {
+              Immich = {
+                description = "Immich";
+                icon = "immich";
+                href = "https://immich.${desktopDomain}";
+                siteMonitor = "https://immich.${desktopDomain}";
+              };
+            })
+            (includeService "paperless" {
+              Paperless = {
+                description = "Paperless";
+                icon = "paperless";
+                href = "https://paperless.${desktopDomain}";
+                siteMonitor = "https://paperless.${desktopDomain}";
+              };
+            })
+            (includeService "gitea" {
+              Gitea = {
+                description = "Gitea";
+                icon = "gitea";
+                href = "https://git.${desktopDomain}";
+                siteMonitor = "https://git.${desktopDomain}";
+              };
+            })
+            (includeService "stump" {
+              Stump = {
+                description = "Stump";
+                icon = "stump";
+                href = "https://books.${desktopDomain}";
+                siteMonitor = "https://books.${desktopDomain}";
+              };
+            })
+            (includeService "maloja" {
+              Maloja = {
+                description = "Maloja";
+                icon = "maloja";
+                href = "https://maloja.${desktopDomain}";
+                siteMonitor = "https://maloja.${desktopDomain}";
+              };
+            })
+            (includeService "audiobookshelf" {
+              Audiobookshelf = {
+                description = "Audiobookshelf";
+                icon = "audiobookshelf";
+                href = "https://audiobooks.${desktopDomain}";
+                siteMonitor = "https://audiobooks.${desktopDomain}";
+              };
+            })
+            (includeService "jellyfin" {
+              Jellyfin = {
+                description = "Jellyfin";
+                icon = "jellyfin";
+                href = "https://jellyfin.${desktopDomain}";
+                siteMonitor = "https://jellyfin.${desktopDomain}";
+              };
+            })
+            (includeService "karakeep" {
+              Karakeep = {
+                description = "Karakeep";
+                icon = "karakeep";
+                href = "https://links.${desktopDomain}";
+                siteMonitor = "https://links.${desktopDomain}";
+              };
+            })
+            (includeService "n8n" {
+              n8n = {
+                description = "n8n";
+                icon = "n8n";
+                href = "https://n8n.${desktopDomain}";
+                siteMonitor = "https://n8n.${desktopDomain}";
+              };
+            })
+            (includeService "nocodb" {
+              NocoDB = {
+                description = "NocoDB";
+                icon = "nocodb";
+                href = "https://nocodb.${desktopDomain}";
+                siteMonitor = "https://nocodb.${desktopDomain}";
+              };
+            })
+          ];
+        }
+      ];
+
+      widgets = [
+        {
+          greeting = {
+            text_size = "xl";
+            text = "Homepage";
+          };
+        }
+        {
+          resources = {
+            cpu = true;
+            memory = true;
+            disk = "/";
+          };
+        }
+        {
+          datetime = {
+            text_size = "xl";
+            format = {
+              dateStyle = "short";
+              timeStyle = "short";
+              hour12 = true;
+            };
           };
         }
       ];
     };
 
-    services = [
-      {
-        "Services" = [
-          {
-            "AdGuard Home" = {
-              description = "AdGuard Home";
-              icon = "adguard-home";
-              href = "https://adguard.${desktopDomain}";
-              siteMonitor = "https://adguard.${desktopDomain}";
-            };
-          }
-          {
-            "OpenWebUI (laptop)" = {
-              description = "OpenWebUI";
-              icon = "ollama";
-              href = "https://ollama.laptop.local";
-              siteMonitor = "https://ollama.laptop.local";
-            };
-          }
-          {
-            "OpenWebUI (server)" = {
-              description = "OpenWebUI";
-              icon = "ollama";
-              href = "https://ollama.${desktopDomain}";
-              siteMonitor = "https://ollama.${desktopDomain}";
-            };
-          }
-          {
-            Miniflux = {
-              description = "Miniflux";
-              icon = "miniflux";
-              href = "https://rss.${desktopDomain}";
-              siteMonitor = "https://rss.${desktopDomain}";
-            };
-          }
-          {
-            Scrutiny = {
-              description = "S.M.A.R.T. Monitoring";
-              icon = "scrutiny";
-              href = "https://scrutiny.${desktopDomain}";
-              siteMonitor = "https://scrutiny.${desktopDomain}";
-            };
-          }
-          {
-            Memos = {
-              description = "Memos";
-              icon = "memos";
-              href = "https://memos.${desktopDomain}";
-              siteMonitor = "https://memos.${desktopDomain}";
-            };
-          }
-          {
-            Netdata = {
-              description = "Netdata";
-              icon = "netdata";
-              href = "https://netdata.${desktopDomain}";
-              siteMonitor = "https://netdata.${desktopDomain}";
-            };
-          }
-          {
-            Immich = {
-              description = "Immich";
-              icon = "immich";
-              href = "https://immich.${desktopDomain}";
-              siteMonitor = "https://immich.${desktopDomain}";
-            };
-          }
-          {
-            Paperless = {
-              description = "Paperless";
-              icon = "paperless";
-              href = "https://paperless.${desktopDomain}";
-              siteMonitor = "https://paperless.${desktopDomain}";
-            };
-          }
-          {
-            Gitea = {
-              description = "Gitea";
-              icon = "gitea";
-              href = "https://git.${desktopDomain}";
-              siteMonitor = "https://git.${desktopDomain}";
-            };
-          }
-          {
-            Stump = {
-              description = "Stump";
-              icon = "stump";
-              href = "https://books.${desktopDomain}";
-              siteMonitor = "https://books.${desktopDomain}";
-            };
-          }
-          {
-            Maloja = {
-              description = "Maloja";
-              icon = "maloja";
-              href = "https://maloja.${desktopDomain}";
-              siteMonitor = "https://maloja.${desktopDomain}";
-            };
-          }
-          {
-            Audiobookshelf = {
-              description = "Audiobookshelf";
-              icon = "audiobookshelf";
-              href = "https://audiobooks.${desktopDomain}";
-              siteMonitor = "https://audiobooks.${desktopDomain}";
-            };
-          }
-          {
-            Jellyfin = {
-              description = "Jellyfin";
-              icon = "jellyfin";
-              href = "https://jellyfin.${desktopDomain}";
-              siteMonitor = "https://jellyfin.${desktopDomain}";
-            };
-          }
-          {
-            Karakeep = {
-              description = "Karakeep";
-              icon = "karakeep";
-              href = "https://links.${desktopDomain}";
-              siteMonitor = "https://links.${desktopDomain}";
-            };
-          }
-          {
-            n8n = {
-              description = "n8n";
-              icon = "n8n";
-              href = "https://n8n.${desktopDomain}";
-              siteMonitor = "https://n8n.${desktopDomain}";
-            };
-          }
-          {
-            NocoDB = {
-              description = "NocoDB";
-              icon = "nocodb";
-              href = "https://nocodb.${desktopDomain}";
-              siteMonitor = "https://nocodb.${desktopDomain}";
-            };
-          }
-        ];
-      }
-    ];
-
-    widgets = [
-      {
-        greeting = {
-          text_size = "xl";
-          text = "Homepage";
-        };
-      }
-      {
-        resources = {
-          cpu = true;
-          memory = true;
-          disk = "/";
-        };
-      }
-      {
-        datetime = {
-          text_size = "xl";
-          format = {
-            dateStyle = "short";
-            timeStyle = "short";
-            hour12 = true;
-          };
-        };
-      }
-    ];
+    services.caddy.virtualHosts."home.${domain}".extraConfig = ''
+      reverse_proxy http://127.0.0.1:7676
+    '';
   };
-
-  services.caddy.virtualHosts."home.${domain}".extraConfig = ''
-    reverse_proxy http://127.0.0.1:7676
-  '';
 }
