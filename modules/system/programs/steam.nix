a3a432e3bcf3de8dae25b309b6ad0d27fe19aec5
not-matthias <26800596+not-matthias@users.noreply.github.com>
2025-10-04 00:47:14 +0200

    chore: update steam config

--- modules/system/programs/steam.nix
+++ modules/system/programs/steam.nix
@@ -10,7 +10,29 @@ in {
     # This has to be set by the user:
     # programs.steam.enable = true;
 
-    programs.gamemode.enable = true;
+    # TODO: Set for the user:
+    # programs.lutris = {
+    #   enable = true;
+    #   package = unstable.lutris;
+    #   protonPackages = with pkgs; [proton-ge-bin];
+    #   winePackages = with pkgs; [
+    #     wineWow64Packages.stable
+    #     wineWowPackages.stagingFull
+    #   ];
+    #   extraPackages = with pkgs; [
+    #     mangohud
+    #     winetricks
+    #     gamescope
+    #     gamemode
+    #     umu-launcher
+    #   ];
+    # };
+
+    programs.gamemode = {
+      enable = true;
+      enableRenice = true;
+    };
+
     programs.gamescope = {
       enable = true;
       capSysNice = true;
@@ -20,12 +42,24 @@ in {
       ];
     };
 
+    # Enable hardware accelerated graphics
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+
     environment.systemPackages = with pkgs; [
       protonup-qt
-      lutris
+      gamemode
 
       wineWowPackages.stable
+      wineWowPackages.waylandFull
       winetricks
+
+      # Vulkan tools and drivers
+      vulkan-tools
+      vulkan-loader
+      vulkan-validation-layers
     ];
 
     # Additional environment variables for gamescope
