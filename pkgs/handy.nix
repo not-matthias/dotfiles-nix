bfd1639f63f99d1c1bd0bfd37efc06e7af06a987
not-matthias <26800596+not-matthias@users.noreply.github.com>
2025-10-18 13:33:38 +0200

    feat(handy): update to latest version

--- pkgs/handy.nix
+++ pkgs/handy.nix
@@ -4,10 +4,10 @@
   fetchurl,
 }: let
   pname = "handy";
-  version = "0.5.0";
+  version = "0.5.3";
   src = fetchurl {
     url = "https://github.com/cjpais/Handy/releases/download/v${version}/Handy_${version}_amd64.AppImage";
-    hash = "sha256-txWaOFB46lPbXFWSezFwdszVEo8ZsGttIH/unALsSgk=";
+    hash = "sha256-A6d/EijWXSZvxibNXa6/oiFQqTG5+dp8z6i2Xr4awtk=";
   };
 in
   appimageTools.wrapType2 {
