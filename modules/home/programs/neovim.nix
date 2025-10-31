9a9a718967c819c34c8611bd1911b30683c506ac
not-matthias <26800596+not-matthias@users.noreply.github.com>
2025-10-31 19:04:09 +0100

    feat(neovim): enhance key mappings for navigation, window management, and editor actions; add session management and code actions UI

--- modules/home/programs/neovim.nix
+++ modules/home/programs/neovim.nix
@@ -25,14 +25,462 @@
       shiftwidth = 4;
     };
     keymaps = [
-      # {
-      #   mode = "t";
-      #   key = "<C-/>";
-      #   action = "<cmd>close<cr>";
-      #   options = {
-      #     desc = "Hide Terminal";
-      #   };
-      # }
+      # ============================================================================
+      # CODE NAVIGATION (JetBrains style)
+      # ============================================================================
+      {
+        mode = "n";
+        key = "gd";
+        action = "<cmd>Telescope lsp_definitions<cr>";
+        options = {
+          silent = true;
+          desc = "Go to definition";
+        };
+      }
+      {
+        mode = "n";
+        key = "<C-b>";
+        action = "<cmd>Telescope lsp_definitions<cr>";
+        options = {
+          silent = true;
+          desc = "Go to definition (Ctrl+B)";
+        };
+      }
+      {
+        mode = "n";
+        key = "gD";
+        action = "<cmd>Telescope lsp_declarations<cr>";
+        options = {
+          silent = true;
+          desc = "Go to declaration";
+        };
+      }
+      {
+        mode = "n";
+        key = "gi";
+        action = "<cmd>Telescope lsp_implementations<cr>";
+        options = {
+          silent = true;
+          desc = "Go to implementation";
+        };
+      }
+      {
+        mode = "n";
+        key = "gy";
+        action = "<cmd>Telescope lsp_type_definitions<cr>";
+        options = {
+          silent = true;
+          desc = "Go to type definition";
+        };
+      }
+      {
+        mode = "n";
+        key = "<C-A-Left>";
+        action = "<cmd>execute 'normal! ' . v:count1 . '<C-o>'<cr>";
+        options = {
+          silent = true;
+          desc = "Navigate back";
+        };
+      }
+      {
+        mode = "n";
+        key = "<C-A-Right>";
+        action = "<cmd>execute 'normal! ' . v:count1 . '<C-i>'<cr>";
+        options = {
+          silent = true;
+          desc = "Navigate forward";
+        };
+      }
+      {
+        mode = "n";
+        key = "<S-F12>";
+        action = "<cmd>Telescope lsp_references<cr>";
+        options = {
+          silent = true;
+          desc = "Find usages/references";
+        };
+      }
+      {
+        mode = "n";
+        key = "<C-h>";
+        action = "<cmd>Trouble lsp<cr>";
+        options = {
+          silent = true;
+          desc = "Show call hierarchy";
+        };
+      }
+
+      # ============================================================================
+      # WINDOW MANAGEMENT (remapped from Ctrl+W to Space+W)
+      # ============================================================================
+      {
+        mode = "n";
+        key = "<space>ww";
+        action = "<C-w>w";
+        options = {
+          silent = true;
+          desc = "Switch window";
+        };
+      }
+      {
+        mode = "n";
+        key = "<space>wh";
+        action = "<C-w>h";
+        options = {
+          silent = true;
+          desc = "Move to left window";
+        };
+      }
+      {
+        mode = "n";
+        key = "<space>wj";
+        action = "<C-w>j";
+        options = {
+          silent = true;
+          desc = "Move to below window";
+        };
+      }
+      {
+        mode = "n";
+        key = "<space>wk";
+        action = "<C-w>k";
+        options = {
+          silent = true;
+          desc = "Move to above window";
+        };
+      }
+      {
+        mode = "n";
+        key = "<space>wl";
+        action = "<C-w>l";
+        options = {
+          silent = true;
+          desc = "Move to right window";
+        };
+      }
+      {
+        mode = "n";
+        key = "<space>wd";
+        action = "<C-w>c";
+        options = {
+          silent = true;
+          desc = "Delete/close window";
+        };
+      }
+      {
+        mode = "n";
+        key = "<space>wv";
+        action = "<C-w>v";
+        options = {
+          silent = true;
+          desc = "Split window vertically";
+        };
+      }
+      {
+        mode = "n";
+        key = "<space>ws";
+        action = "<C-w>s";
+        options = {
+          silent = true;
+          desc = "Split window horizontally";
+        };
+      }
+      {
+        mode = "n";
+        key = "<C-w>d";
+        action = "<nop>";
+        options = {
+          silent = true;
+          desc = "Unbind diagnostic shortcut";
+        };
+      }
+      {
+        mode = "n";
+        key = "<C-w>c";
+        action = "<nop>";
+        options = {
+          silent = true;
+          desc = "Unbind diagnostic shortcut";
+        };
+      }
+
+      # ============================================================================
+      # CODE ACTIONS & REFACTORING
+      # ============================================================================
+      {
+        mode = "n";
+        key = "<F2>";
+        action = "<cmd>lua vim.lsp.buf.rename()<cr>";
+        options = {
+          silent = true;
+          desc = "Rename symbol";
+        };
+      }
+      {
+        mode = "n";
+        key = "<S-F6>";
+        action = "<cmd>lua vim.lsp.buf.rename()<cr>";
+        options = {
+          silent = true;
+          desc = "Rename symbol (Shift+F6)";
+        };
+      }
+      {
+        mode = "n";
+        key = "<C-A-l>";
+        action = "<cmd>lua vim.lsp.buf.format()<cr>";
+        options = {
+          silent = true;
+          desc = "Format document";
+        };
+      }
+      {
+        mode = "v";
+        key = "<C-A-l>";
+        action = "<cmd>lua vim.lsp.buf.format()<cr>";
+        options = {
+          silent = true;
+          desc = "Format selection";
+        };
+      }
+      {
+        mode = "n";
+        key = "<C-A-o>";
+        action = "<cmd>Lspsaga code_action<cr>";
+        options = {
+          silent = true;
+          desc = "Organize imports / code actions";
+        };
+      }
+
+      # ============================================================================
+      # SEARCH & FIND
+      # ============================================================================
+      {
+        mode = "n";
+        key = "<C-o>";
+        action = "<cmd>Telescope find_files<cr>";
+        options = {
+          silent = true;
+          desc = "Open file";
+        };
+      }
+      {
+        mode = "n";
+        key = "<C-S-f>";
+        action = "<cmd>Telescope live_grep<cr>";
+        options = {
+          silent = true;
+          desc = "Find in files";
+        };
+      }
+      {
+        mode = "n";
+        key = "<C-f>";
+        action = "<cmd>Telescope current_buffer_fuzzy_find<cr>";
+        options = {
+          silent = true;
+          desc = "Find in current buffer";
+        };
+      }
+      {
+        mode = "n";
+        key = "<C-S-n>";
+        action = "<cmd>Telescope lsp_document_symbols<cr>";
+        options = {
+          silent = true;
+          desc = "Find symbol in buffer";
+        };
+      }
+      {
+        mode = "n";
+        key = "<C-S-a>";
+        action = "<cmd>Telescope commands<cr>";
+        options = {
+          silent = true;
+          desc = "Find action (command palette)";
+        };
+      }
+
+      # ============================================================================
+      # EDITOR ACTIONS
+      # ============================================================================
+      {
+        mode = "n";
+        key = "<C-d>";
+        action = "<cmd>normal! yyp<cr>";
+        options = {
+          silent = true;
+          desc = "Duplicate line";
+        };
+      }
+      {
+        mode = "v";
+        key = "<C-d>";
+        action = "<cmd>normal! y`>p<cr>";
+        options = {
+          silent = true;
+          desc = "Duplicate selection";
+        };
+      }
+      {
+        mode = "n";
+        key = "<C-/>";
+        action = "<Plug>(comment_toggle_linewise_current)";
+        options = {
+          desc = "Toggle line comment";
+        };
+      }
+      {
+        mode = "v";
+        key = "<C-/>";
+        action = "<Plug>(comment_toggle_linewise_visual)";
+        options = {
+          desc = "Toggle block comment";
+        };
+      }
+
+      # ============================================================================
+      # TAB/BUFFER MANAGEMENT
+      # ============================================================================
+      {
+        mode = "n";
+        key = "<C-Tab>";
+        action = "<cmd>BufferLineCycleNext<cr>";
+        options = {
+          silent = true;
+          desc = "Next buffer/tab";
+        };
+      }
+      {
+        mode = "n";
+        key = "<C-S-Tab>";
+        action = "<cmd>BufferLineCyclePrev<cr>";
+        options = {
+          silent = true;
+          desc = "Previous buffer/tab";
+        };
+      }
+      {
+        mode = "n";
+        key = "<C-t>";
+        action = "<cmd>enew<cr>";
+        options = {
+          silent = true;
+          desc = "New buffer/tab";
+        };
+      }
+      {
+        mode = "n";
+        key = "<C-w>";
+        action = "<cmd>bdelete<cr>";
+        options = {
+          silent = true;
+          desc = "Close buffer/tab";
+        };
+      }
+      {
+        mode = "n";
+        key = "<C-S-t>";
+        action = "<cmd>BufferLineGroupClose ungrouped<cr>";
+        options = {
+          silent = true;
+          desc = "Reopen closed buffer";
+        };
+      }
+
+      # ============================================================================
+      # TERMINAL
+      # ============================================================================
+      {
+        mode = "n";
+        key = "<C-`>";
+        action = "<cmd>ToggleTerm<cr>";
+        options = {
+          silent = true;
+          desc = "Toggle terminal";
+        };
+      }
+      {
+        mode = "t";
+        key = "<C-`>";
+        action = "<cmd>ToggleTerm<cr>";
+        options = {
+          silent = true;
+          desc = "Toggle terminal (from terminal mode)";
+        };
+      }
+      {
+        mode = "n";
+        key = "<C-F12>";
+        action = "<cmd>ToggleTerm<cr>";
+        options = {
+          silent = true;
+          desc = "Toggle terminal";
+        };
+      }
+      {
+        mode = "t";
+        key = "<C-F12>";
+        action = "<cmd>ToggleTerm<cr>";
+        options = {
+          silent = true;
+          desc = "Toggle terminal (from terminal mode)";
+        };
+      }
+      {
+        mode = "n";
+        key = "<C-S-F12>";
+        action = "<cmd>ToggleTerm size=40 direction=float<cr>";
+        options = {
+          silent = true;
+          desc = "New floating terminal";
+        };
+      }
+
+      # ============================================================================
+      # TOOL WINDOWS (using Ctrl since Alt is reserved for window manager)
+      # ============================================================================
+      {
+        mode = "n";
+        key = "<C-1>";
+        action = "<cmd>NvimTreeToggle<cr>";
+        options = {
+          silent = true;
+          desc = "Toggle file explorer";
+        };
+      }
+      {
+        mode = "n";
+        key = "<C-9>";
+        action = "<cmd>Neogit<cr>";
+        options = {
+          silent = true;
+          desc = "Toggle Git (Neogit)";
+        };
+      }
+      {
+        mode = "n";
+        key = "<C-a>";
+        action = "<cmd>AerialToggle<cr>";
+        options = {
+          silent = true;
+          desc = "Toggle symbol outline";
+        };
+      }
+      {
+        mode = "n";
+        key = "<C-u>";
+        action = "<cmd>UndotreeToggle<cr>";
+        options = {
+          silent = true;
+          desc = "Toggle undo tree";
+        };
+      }
+
+      # ============================================================================
+      # LEGACY SPACE-BASED KEYBINDS (preserved for compatibility)
+      # ============================================================================
       {
         mode = "n";
         key = "<space>fb";
@@ -95,10 +543,26 @@
       bufferline.enable = true;
       toggleterm.enable = true;
 
+      # Session management
+      auto-session = {
+        enable = true;
+        settings = {
+          auto_save_enabled = true;
+          auto_restore_enabled = true;
+          auto_session_suppress_dirs = [
+            "~/"
+            "~/Projects"
+            "/tmp"
+          ];
+        };
+      };
+
       # QoL
+      easyescape.enable = true;
       lastplace.enable = true;
       snacks.enable = true;
       undotree.enable = true;
+      dashboard.enable = true;
       aerial = {
         enable = true;
         settings = {
@@ -232,6 +696,9 @@
         };
       };
 
+      # Code actions and refactoring UI
+      lspsaga.enable = true;
+
       conform-nvim = {
         enable = true;
         settings = {
@@ -303,6 +770,9 @@
       blink-compat.enable = true;
 
       nix.enable = true;
+
+      # Claude Code integration
+      # claude-code.enable = true;
 
       # git
       trouble.enable = true;
