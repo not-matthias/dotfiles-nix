3a63474f5b4dd02758f2b8c9c69cfcf5173a753a
not-matthias <26800596+not-matthias@users.noreply.github.com>
2026-01-15 00:01:37 +0100

    fix(atuin): use directory filter_mode

--- modules/home/programs/atuin.nix
+++ modules/home/programs/atuin.nix
@@ -5,6 +5,7 @@
     enableFishIntegration = false;
     settings = {
       search_mode = "skim"; # fuzzy: doesn't quite work for me. example: search for env var i set before
+      filter_mode = "directory";
       keymap_mode = "vim-insert";
       secrets_filter = true;
       store_failed = false;
