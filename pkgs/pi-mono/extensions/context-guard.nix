9645240b9baa7ee744645c1bd49c12e55a0f81da
not-matthias <matthias@codspeed.io>
2026-04-09 20:29:35 +0200

    feat(pi-mono): add context-guard extension

---
+++ pkgs/pi-mono/extensions/context-guard.nix
@@ -0,0 +1,7 @@
+{fetchFromGitHub}:
+fetchFromGitHub {
+  owner = "emanuelcasco";
+  repo = "pi-mono-extensions";
+  rev = "1ddb665bf9d07ff0a54bd19dbd7c6516a750631a";
+  hash = "sha256-KflCjU0uwMQCNEdxqghA55wZHJI2kdqfpXkVcr7sOUg=";
+}
