3b81abf3197da239335a8c5e3c43186d92997a2f
not-matthias <matthias@codspeed.io>
2026-04-09 20:29:39 +0200

    feat(pi-mono): add pi-fff extension

---
+++ pkgs/pi-mono/extensions/pi-fff.nix
@@ -0,0 +1,7 @@
+{fetchFromGitHub}:
+fetchFromGitHub {
+  owner = "ShpetimA";
+  repo = "pi-fff";
+  rev = "a25b49a54fea9a017bb616dceeb6b6d156add4f1";
+  hash = "sha256-U1KxeRS7UxWVySM3ZTEz6PVEqSQQQksm4tyEbm3c59M=";
+}
