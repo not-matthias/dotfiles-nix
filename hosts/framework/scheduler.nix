68b90591a2b41e573141d2d00db14dff7d39a0f6
not-matthias <matthias@codspeed.io>
2026-05-03 21:33:51 +0200

    chore(framework): disable cpufreq and local-kthreads scx flags

--- hosts/framework/scheduler.nix
+++ hosts/framework/scheduler.nix
@@ -10,13 +10,13 @@
       "--no-wake-sync" # -w
       # Enable CPU frequency control (only with schedutil governor).
       # With this option enabled the CPU frequency will be automatically scaled based on the load.
-      "--cpufreq" # -f
+      # "--cpufreq" # -f
       # Enable per-CPU tasks prioritization.
       # This allows to prioritize per-CPU tasks that usually tend to be de-prioritized (since they can't be migrated when their only usable CPU is busy). Enabling this option can introduce unfairness and potentially trigger stalls, but it can improve performance of server-type workloads (such as large parallel builds).
       "--local-pcpu" # -p
       # Enable kthreads prioritization (EXPERIMENTAL).
       # Enabling this can improve system performance, but it may also introduce noticeable interactivity issues or unfairness in scenarios with high kthread activity, such as heavy I/O or network traffic. Use it only when conducting specific experiments or if you have a clear understanding of its implications.
-      "--local-kthreads" # -k
+      # "--local-kthreads" # -k
     ];
   };
 }
