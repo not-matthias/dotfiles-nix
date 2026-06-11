a309884eb76a8f4ad4067ac14fdac1b65c85f363
not-matthias <matthias@codspeed.io>
2026-06-11 14:09:20 +0200

    feat(work): add SSH config for codspeed mac-mini

--- hosts/framework/work.nix
+++ hosts/framework/work.nix
@@ -9,6 +9,20 @@
       slack
       awscli2
     ];
+
+    programs.ssh.matchBlocks."codspeeds-mac-mini" = {
+      host = "codspeeds-mac-mini codspeeds-mac-mini.tail0bdeec.ts.net";
+      hostname = "codspeeds-mac-mini.tail0bdeec.ts.net";
+      user = "codspeed";
+      forwardAgent = true;
+      identitiesOnly = true;
+      remoteForwards = [
+        {
+          host.address = "/run/user/1000/gnupg/S.gpg-agent.extra";
+          bind.address = "/Users/codspeed/.gnupg/S.gpg-agent";
+        }
+      ];
+    };
   };
 
   # Enables the 1Password CLI
