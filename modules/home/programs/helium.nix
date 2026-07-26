82014bc573cf67bb696fb179678342bb07b891e4
not-matthias <matthias@codspeed.io>
2026-07-26 15:49:03 +0200

    feat: enable hardware accel and autoscroll flags in Helium

--- modules/home/programs/helium.nix
+++ modules/home/programs/helium.nix
@@ -6,6 +6,8 @@
       "--no-default-browser-check"
       "--disable-breakpad"
       "--restore-last-session"
+      "--enable-blink-features=MiddleClickAutoscroll"
+      "--enable-features=AcceleratedVideoEncoder,ParallelDownloading,Vulkan,VulkanFromANGLE,DefaultANGLEVulkan,PlatformHEVCDecoderSupport"
     ];
 
     extensions = [
