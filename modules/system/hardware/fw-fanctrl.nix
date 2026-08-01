d3a973bf991a09370523dae8caf8a3d36e762636
not-matthias <matthias@codspeed.io>
2026-08-01 14:44:46 +0200

    fix: isolate fw-fanctrl Python dependencies

--- modules/system/hardware/fw-fanctrl.nix
+++ modules/system/hardware/fw-fanctrl.nix
@@ -1,12 +1,26 @@
 {
   config,
   lib,
+  pkgs,
   unstable,
   ...
 }: {
   # Override the package to use unstable version
   config = lib.mkIf config.hardware.fw-fanctrl.enable {
-    hardware.fw-fanctrl.package = lib.mkDefault unstable.fw-fanctrl;
+    # Keep a shell's Python package paths from shadowing this application's closure.
+    hardware.fw-fanctrl.package = lib.mkDefault (pkgs.symlinkJoin {
+      name = "fw-fanctrl";
+      meta =
+        unstable.fw-fanctrl.meta
+        // {
+          mainProgram = "fw-fanctrl";
+        };
+      paths = [unstable.fw-fanctrl];
+      nativeBuildInputs = [pkgs.makeWrapper];
+      postBuild = ''
+        wrapProgram $out/bin/fw-fanctrl --unset PYTHONPATH
+      '';
+    });
     hardware.fw-fanctrl.ectoolPackage = lib.mkDefault unstable.fw-ectool;
   };
 }
