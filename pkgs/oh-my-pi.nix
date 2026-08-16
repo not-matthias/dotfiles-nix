0999f7f8fd749b3e9ddb768b1333904d1b0c2c02
not-matthias <matthias@codspeed.io>
2026-08-16 14:52:38 +0200

    chore(omp): update to 17.3.5
    Update the packaged OMP release binary and its verified SHA-256 hash.

--- pkgs/oh-my-pi.nix
+++ pkgs/oh-my-pi.nix
@@ -9,10 +9,10 @@
 # refuses to start when the system Bun is older than its MIN_BUN_VERSION).
 stdenv.mkDerivation rec {
   pname = "oh-my-pi";
-  version = "17.3.4";
+  version = "17.3.5";
   src = fetchurl {
     url = "https://github.com/can1357/oh-my-pi/releases/download/v${version}/omp-linux-x64";
-    hash = "sha256-P85LJWKAZLDNe/vGJF7NraMxdQ7Us0Gspr0pukR4qrU=";
+    hash = "sha256-YFtKijoTdImpHVnnAoxB86/yAWnzUpArQQifv9+KJTw=";
   };
 
   dontUnpack = true;
