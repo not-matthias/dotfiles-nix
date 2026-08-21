2767efd0faab9cc1877dacaa4b0589427adf0b0c
not-matthias <matthias@codspeed.io>
2026-08-21 19:30:18 +0200

    chore: bump herdr mirror plugin to 0.3.1

--- pkgs/herdr-plugins/mirror.nix
+++ pkgs/herdr-plugins/mirror.nix
@@ -4,15 +4,15 @@
   fetchurl,
   ...
 }: let
-  version = "0.2.2";
+  version = "0.3.1";
   assets = {
     x86_64-linux = {
       name = "herdr-mirror-linux-x86_64";
-      hash = "sha256-VXiyjhphlX077lo5YsJrj9QR03P1pVnkiV0/JdSk6rs=";
+      hash = "sha256-jW5Y+sa2A8A1b3d6CbgFW7lnkk9++UfxDFSrmkcaLGA=";
     };
     aarch64-linux = {
       name = "herdr-mirror-linux-aarch64";
-      hash = "sha256-lXD3NT2hlZdTfuIuVirq3u/0We/ZgvQt2JH6gXGt4LI=";
+      hash = "sha256-TkBGFQWeGn26E54+oOhLNBkWOgfSsfKvqxTXkIAWk+E=";
     };
   };
   asset =
