f665f157eb74d5d16eea51bcd6c739aeb9253f90
not-matthias <matthias@codspeed.io>
2026-08-22 15:20:27 +0200

    chore(omp): bump packaged release to 17.4.2
    Resolve the interrupted package update by retaining the newer verified release and its matching binary hash. This clears the unmerged index state and keeps the PC build on a concrete OMP asset.

--- pkgs/oh-my-pi.nix
+++ pkgs/oh-my-pi.nix
@@ -9,10 +9,10 @@
 # refuses to start when the system Bun is older than its MIN_BUN_VERSION).
 stdenv.mkDerivation rec {
   pname = "oh-my-pi";
-  version = "17.4.1";
+  version = "17.4.2";
   src = fetchurl {
     url = "https://github.com/can1357/oh-my-pi/releases/download/v${version}/omp-linux-x64";
-    hash = "sha256-XsUef20Hj8/IGNfgnz94RjtJTscmh+JGMdlPHtk/AzU=";
+    hash = "sha256-IYqGhMKxEla0fii6ExrfsqA+mI7d2FZ72Da3xR3QIAU=";
   };
 
   dontUnpack = true;
