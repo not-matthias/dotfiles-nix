7cd78663676476437326d00c7b7539a498b3a06b
not-matthias <matthias@codspeed.io>
2026-08-23 23:55:55 +0200

    chore(atuin): switch search mode to daemon-fuzzy
    Update atuin search_mode configuration from skim to daemon-fuzzy.

--- modules/home/programs/atuin.nix
+++ modules/home/programs/atuin.nix
@@ -4,7 +4,7 @@
     package = unstable.atuin;
     enableFishIntegration = false;
     settings = {
-      search_mode = "skim"; # fuzzy: doesn't quite work for me. example: search for env var i set before
+      search_mode = "daemon-fuzzy"; # fuzzy: doesn't quite work for me. example: search for env var i set before
       filter_mode = "directory";
       keymap_mode = "vim-insert";
       secrets_filter = true;
