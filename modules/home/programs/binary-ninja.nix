91d4f5954c7ce230bc1745433ec106213ffe719a
not-matthias <matthias@codspeed.io>
2026-08-30 22:50:48 +0200

    fix(binary-ninja): disable automatic updates
    Manage Binary Ninja's network.enableUpdates setting through the Home Manager activation hook. Merge the setting into the writable user settings file so existing preferences are preserved and the runtime cache remains unaffected.

--- modules/home/programs/binary-ninja.nix
+++ modules/home/programs/binary-ninja.nix
@@ -5,6 +5,10 @@
   ...
 }: let
   cfg = config.programs.binary-ninja;
+  desiredSettings = builtins.toJSON {
+    "network.enableUpdates" = false;
+  };
+  settingsFile = pkgs.writeText "binaryninja-settings.json" desiredSettings;
 in {
   options.programs.binary-ninja = {
     enable = lib.mkEnableOption "Binary Ninja reverse engineering platform";
@@ -25,6 +29,19 @@ in {
 
   config = lib.mkIf cfg.enable {
     home.packages = [cfg.package] ++ cfg.extensions;
+
+    home.activation.binaryNinjaSettings = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      target="$HOME/.binaryninja/settings.json"
+      if [[ -e "$target" && ! -L "$target" ]]; then
+        temporary="$target.tmp"
+        ${pkgs.jq}/bin/jq --argjson desired '${desiredSettings}' '. * $desired' "$target" > "$temporary"
+        $DRY_RUN_CMD install -m 0644 "$temporary" "$target"
+        $DRY_RUN_CMD rm -f "$temporary"
+      else
+        $DRY_RUN_CMD rm -f "$target"
+        $DRY_RUN_CMD install -Dm 0644 ${settingsFile} "$target"
+      fi
+    '';
 
     # Keep this directory writable because Binary Ninja stores plugin manager state here.
     home.file.".binaryninja/plugins" = lib.mkIf (cfg.extensions != []) {
