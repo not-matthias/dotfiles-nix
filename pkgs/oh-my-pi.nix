588385b804f1a8a0beb74ea609f980ad49e085c4
not-matthias <matthias@codspeed.io>
2026-09-05 22:21:07 +0200

    chore(omp): bump package to 18.1.11
    Update the packaged OMP release and its fixed-output source hash.

--- pkgs/oh-my-pi.nix
+++ pkgs/oh-my-pi.nix
@@ -10,10 +10,10 @@
 # refuses to start when the system Bun is older than its MIN_BUN_VERSION).
 stdenv.mkDerivation rec {
   pname = "oh-my-pi";
-  version = "18.1.10";
+  version = "18.1.11";
   src = fetchurl {
     url = "https://github.com/can1357/oh-my-pi/releases/download/v${version}/omp-linux-x64";
-    hash = "sha256-6R1VmO5H4dQJn9hobcn2HJt1Xy6gd9Xxd0q6EHIyH54=";
+    hash = "sha256-Kyx4W7uv07Q/tgluSuY6A0LE2Aad4vh1qbduqZR8I4M=";
   };
 
   dontUnpack = true;
