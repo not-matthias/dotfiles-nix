28c0eee7707855b6d00cb81ee57aa1973d68d582
not-matthias <matthias@codspeed.io>
2026-09-17 11:51:20 +0200

    chore: bump omp to 18.2.4
    Update the standalone release binary and its content hash to the latest upstream version.

--- pkgs/oh-my-pi.nix
+++ pkgs/oh-my-pi.nix
@@ -10,10 +10,10 @@
 # refuses to start when the system Bun is older than its MIN_BUN_VERSION).
 stdenv.mkDerivation rec {
   pname = "oh-my-pi";
-  version = "18.2.3";
+  version = "18.2.4";
   src = fetchurl {
     url = "https://github.com/can1357/oh-my-pi/releases/download/v${version}/omp-linux-x64";
-    hash = "sha256-+FvftKdj0zmd3RhBP4gLJrNtp2Wqdb1fzH0lIjnH8Xo=";
+    hash = "sha256-YbTNUM6upwusyue1KiIDRGkTDqKYWgsumtwPezp3qF8=";
   };
 
   dontUnpack = true;
