b6ca6b0e7f6b2eae056f913a6d2a1b9658f8f19c
not-matthias <matthias@codspeed.io>
2026-09-21 10:40:59 +0200

    chore: bump omp to 18.2.7

--- pkgs/oh-my-pi.nix
+++ pkgs/oh-my-pi.nix
@@ -10,10 +10,10 @@
 # refuses to start when the system Bun is older than its MIN_BUN_VERSION).
 stdenv.mkDerivation rec {
   pname = "oh-my-pi";
-  version = "18.2.6";
+  version = "18.2.7";
   src = fetchurl {
     url = "https://github.com/can1357/oh-my-pi/releases/download/v${version}/omp-linux-x64";
-    hash = "sha256-DzhZjJHoI9jM4H8VHsOZnVHyE/ssw+B9ifGvju+SR6I=";
+    hash = "sha256-HCjtZlZuyrq4FFFM/mJrSv9ZgbfKmJJHe0ZVD+a22Ds=";
   };
 
   dontUnpack = true;
